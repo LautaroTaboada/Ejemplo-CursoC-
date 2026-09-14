bff767325b73999a99ec5b9cd68b34cbae7439ce
unknown <taboaa2010@gmail.com>
2026-09-14 15:27:37 -0300

    Subir

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //variables 
+            //int, float, bool, char
+            //double, decimal, long, short, string, datetime
+            int a, b, c;
+            float d, f;
+            Console.WriteLine("Hola como va?");
+            Console.WriteLine("Ingrese un nro");
+            a = int.Parse(Console.ReadLine()); //casteo explicito
+            b = 10; 
+            c = a + b;
+            Console.WriteLine("El resultado es: " + c);
+            Console.ReadKey();
+             
+        }
+    }
+}
